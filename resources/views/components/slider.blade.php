7fa4b1289c5f7831f67f2410f9a6e8c9d1f944ca
imoreno73 <92292636+imoreno73@users.noreply.github.com>
2022-02-01 12:24:41 +0100

    added & changued images

--- resources/views/components/slider.blade.php
+++ resources/views/components/slider.blade.php
@@ -2,13 +2,13 @@
       <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active" data-bs-interval="10000">
-            <img src="{{asset('assets/img/imagen1.jpg')}}" class="d-block w-5" alt="...">
+            <img src="{{asset('assets/img/computer-gbf0eca64f_1920.jpg')}}" class="d-block w-10" alt="...">
           </div>
           <div class="carousel-item" data-bs-interval="2000">
-            <img src="{{asset('assets/img/imagen2.jpg')}}" class="d-block w-10" alt="...">
+            <img src="{{asset('assets/img/vr-gefde414df_1920.jpg')}}" class="d-block w-10" alt="...">
           </div>
           <div class="carousel-item">
-            <img src="{{asset('assets/img/imagen3.jpg')}}" class="d-block w-10" alt="...">
+            <img src="{{asset('assets/img/hand-gfb55a6bf5_1920.jpg')}}" class="d-block w-10" alt="...">
           </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
