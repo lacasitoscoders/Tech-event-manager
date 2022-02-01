a48848d7588dc5ee858e1517fae781ff2e41891f
imoreno73 <92292636+imoreno73@users.noreply.github.com>
2022-02-01 14:46:10 +0100

    center slider

--- resources/views/components/slider.blade.php
+++ resources/views/components/slider.blade.php
@@ -1,14 +1,14 @@
 <div>
-      <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+      <div id="carouselExampleInterval" class="carousel slide ml-3 mr-3" data-bs-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active" data-bs-interval="10000">
-            <img src="{{asset('assets/img/computer-gbf0eca64f_1920.jpg')}}" class="d-block w-10" alt="...">
+            <img src="{{asset('assets/img/computer-gbf0eca64f_1920.jpg')}}" class="img-fluid mx-auto col-sm-6 rounded d-sm-block" alt="...">
           </div>
           <div class="carousel-item" data-bs-interval="2000">
-            <img src="{{asset('assets/img/vr-gefde414df_1920.jpg')}}" class="d-block w-10" alt="...">
+            <img src="{{asset('assets/img/vr-gefde414df_1920.jpg')}}" class="img-fluid mx-auto col-sm-6 rounded d-sm-block" alt="...">
           </div>
           <div class="carousel-item">
-            <img src="{{asset('assets/img/hand-gfb55a6bf5_1920.jpg')}}" class="d-block w-10" alt="...">
+            <img src="{{asset('assets/img/hand-gfb55a6bf5_1920.jpg')}}" class="img-fluid mx-auto col-sm-6 rounded d-sm-block" alt="...">
           </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
