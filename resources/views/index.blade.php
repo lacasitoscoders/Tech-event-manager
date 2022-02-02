472a5efad24143481ad33365e5f66b0d52fe1714
UxerDevCoding <omarheviaarbana@gmail.com>
2022-02-02 12:32:44 +0100

    develop up to date and problems resolved

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,23 +1,9 @@
 @extends('layouts.web')
 
-<<<<<<< HEAD
-<x-slider/>
-@section('content')
-    <div>
-<<<<<<< HEAD
-        
-        <h1 class="primary">OMAR I EL TIRANO</h1>
-
-=======
-        <h1 class="primary">Ruta view index)</h1>
->>>>>>> feature/controllers
-    </div>
-=======
 
 @section('content')
         <div>
             <h1 class="primary">OMAR I EL TIRANO</h1>
         </div>
 
->>>>>>> 47f125fbe95dc182dacff94dada80042f52e1875
 @endsection
