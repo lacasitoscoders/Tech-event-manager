be314833e425e6041d7c81395e2d1c08559abae5
UxerDevCoding <omarheviaarbana@gmail.com>
2022-02-14 15:10:16 +0100

    function register and boot modified in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,17 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Routing\UrlGenerator;
+
 
 class AppServiceProvider extends ServiceProvider
 {
+    public function boot(UrlGenerator $url)
+    {
+       if (env('REDIRECT_HTTPS')) {
+           $url->formatScheme('https://');
+       }
+    }
     /**
      * Register any application services.
      *
@@ -14,16 +22,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        Schema::defaultStringLength(191);
-    }
+   
+    // public function boot()
+    // {
+    //     Schema::defaultStringLength(191);
+    // }
 }
